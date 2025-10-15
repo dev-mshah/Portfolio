debb34058250a5701edae0c72aa0072b3c55bfe6
user <user@users-MacBook-Pro.local>
2025-10-14 23:15:17 -0500

    Updated projects

---
+++ src/app/avatar/avatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.css',
+
+})
+export class AvatarComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,13 @@ import { ProjectsComponent } from '../projects/projects.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-
+import { AvatarComponent } from '../avatar/avatar.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ProjectsComponent, MatButtonModule, MatCardModule, MatGridListModule, MatExpansionModule],
+  imports: [ProjectsComponent, MatButtonModule, MatCardModule, MatGridListModule, MatExpansionModule, AvatarComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
